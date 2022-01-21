72aabf95e941b247754e77fa26061f20ca6caa04
Henry Peterson <hgpeterson@sampo.gps.caltech.edu>
2022-01-21 09:41:50 -0800

    histograms and percentiles in one

--- gpcp.py
+++ gpcp.py
@@ -18,11 +18,11 @@ import cmip6_lib
 # functions
 ################################################################################
 
-def get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice):
+def get_precip(dfiles, timeslice, latslice, lonslice):
     """
-        precip, precip_pctls, precip_pctls_boot = get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+        precip = get_precip(dfiles, timeslice, latslice, lonslice) 
 
-    Get the precipitation percentiles from monthly GCPC data.
+    Load GPCP precipitation data.
     """
     # mfdataset for multiple files
     ds = xr.open_mfdataset(dfiles)
@@ -40,7 +40,16 @@ def get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice):
     weights *= np.tile(cosϕ, (len(precip.longitude), 1)).T
     precip *= weights
 
+    return precip
+
+def get_pctls(dfiles, pctls, timeslice, latslice, lonslice):
+    """
+        precip, precip_pctls, precip_pctls_boot = get_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+
+    Get the precipitation percentiles from monthly GCPC data.
+    """
     # load
+    precip = get_precip(dfiles, timeslice, latslice, lonslice)
     precip = precip.values
     
     # compute pctls
@@ -61,33 +70,68 @@ def get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice):
     
     return precip, precip_pctls, precip_pctls_boot
 
+def get_hist(dfiles, nbins, timeslice, latslice, lonslice):
+    """
+        hist, bins = get_hist(dfiles, nbins, timeslice, latslice, lonslice)
+
+    Get the precipitation histogram.
+    """
+    # load 
+    precip = get_precip(dfiles, timeslice, latslice, lonslice)
+
+    # setup bins
+    pmin = 10**-5
+    pmax = 10**3
+    bins = np.zeros(nbins)
+    bins[1:] = 10**np.linspace(np.log10(pmin), np.log10(pmax), nbins-1) # include 0 as a bin
+
+    # compute pdf
+    hist, bins = da.histogram(precip, bins=bins, density=False)    
+
+    # actually compute
+    hist = hist.compute()
+
+    return hist, bins
+
 """
-    get_data_gpcp(dfiles, pctls, latslice, lonslice)
+    get_data(dfiles, pctls, nbins, latslice, lonslice)
 
 Save pctls data fro gpcp for 1979-2000 and 2000-2020.
 """
-def get_data_gpcp(dfiles, pctls, latslice, lonslice):
+def get_data(dfiles, pctls, nbins, latslice, lonslice):
     # control: first 21 years
     print("computing control pctls.")
     timeslice = slice("1979", "2000")
-    precip_c, precip_pctls_c, precip_pctls_boot_c = get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+    print("\tpercentiles")
+    precip_c, precip_pctls_c, precip_pctls_boot_c = get_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+    print("\thist")
+    hist_c, bins_c = get_hist(dfiles, nbins, timeslice, latslice, lonslice)
 
     # warming: last 20 years
     print("computing warming pctls")
     timeslice = slice("2000", "2020")
-    precip_w, precip_pctls_w, precip_pctls_boot_w = get_precip_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+    print("\tpercentiles")
+    precip_w, precip_pctls_w, precip_pctls_boot_w = get_pctls(dfiles, pctls, timeslice, latslice, lonslice)
+    print("\thist")
+    hist_w, bins_w = get_hist(dfiles, nbins, timeslice, latslice, lonslice)
     
     # mean and std
     print("computing mean and std")
     μ_c, σ_c = cmip6_lib.get_mean_std(precip_c)
     μ_w, σ_w = cmip6_lib.get_mean_std(precip_w)
-    
-    np.savez(f"data/gpcp_{domain}_1979-2000.npz", pctls=pctls, precip=precip_c, precip_pctls=precip_pctls_c, precip_pctls_boot=precip_pctls_boot_c, μ=μ_c, σ=σ_c)
-    np.savez(f"data/gpcp_{domain}_2000-2020.npz", pctls=pctls, precip=precip_w, precip_pctls=precip_pctls_w, precip_pctls_boot=precip_pctls_boot_w, μ=μ_w, σ=σ_w)
+
+    np.savez(f"data/gpcp_{domain}_{freq}_1979-2000.npz", pctls=pctls, precip=precip_c, 
+             precip_pctls=precip_pctls_c, precip_pctls_boot=precip_pctls_boot_c, 
+             bins=bins_c, hist=hist_c,
+             μ=μ_c, σ=σ_c)
+    np.savez(f"data/gpcp_{domain}_{freq}_2000-2020.npz", pctls=pctls, precip=precip_w, 
+             precip_pctls=precip_pctls_w, precip_pctls_boot=precip_pctls_boot_w, 
+             bins=bins_w, hist=hist_w,
+             μ=μ_w, σ=σ_w)
 
 def load_data(file):
     """
-        pctls, precip, precip_pctls, precip_pctls_boot, μ, σ, k, θ = load_data(file)
+        pctls, precip, precip_pctls, precip_pctls_boot, bins, hist, μ, σ, k, θ = load_data(file)
 
     Load pctl data from .npz file.
     """
@@ -97,36 +141,39 @@ def load_data(file):
     precip = data["precip"]
     precip_pctls = data["precip_pctls"]
     precip_pctls_boot = data["precip_pctls_boot"]
+    bins = data["bins"]
+    hist = data["hist"]
     μ = data["μ"]
     σ = data["σ"]
 
     # compute gamma fit
     k, θ = cmip6_lib.gamma_moments(μ, σ)
 
-    return pctls, precip, precip_pctls, precip_pctls_boot, μ, σ, k, θ
+    return pctls, precip, precip_pctls, precip_pctls_boot, bins, hist, μ, σ, k, θ
 
 def plot_pctls():
     fig, ax = plt.subplots()
 
-    pctls, precip_c, precip_pctls_c, precip_pctls_boot_c, μ_c, σ_c, k_c, θ_c = load_data(f"data/gpcp_{domain}_1979-2000.npz")
-    pctls, precip_w, precip_pctls_w, precip_pctls_boot_w, μ_w, σ_w, k_w, θ_w = load_data(f"data/gpcp_{domain}_2000-2020.npz")
+    pctls, precip_c, precip_pctls_c, precip_pctls_boot_c, bins_c, hist_c, μ_c, σ_c, k_c, θ_c = load_data(f"data/gpcp_{domain}_{freq}_1979-2000.npz")
+    pctls, precip_w, precip_pctls_w, precip_pctls_boot_w, bins_w, hist_w, μ_w, σ_w, k_w, θ_w = load_data(f"data/gpcp_{domain}_{freq}_2000-2020.npz")
 
     # means and standard deviations from pdfs
     mean_scaling = μ_w/μ_c - 1
 
     # warming fit based on theory
-    i99 = np.argmin(np.abs(pctls - 0.99))
-    extreme_scaling = precip_pctls_w[i99]/precip_pctls_c[i99] - 1
-    # extreme_scaling = precip_pctls_w[-1]/precip_pctls_c[-1] - 1
+    if freq == "monthly":
+        i99 = np.argmin(np.abs(pctls - 0.99))
+        extreme_scaling = precip_pctls_w[i99]/precip_pctls_c[i99] - 1
+    else:
+        extreme_scaling = precip_pctls_w[-1]/precip_pctls_c[-1] - 1
     θ_w_theory = θ_c*(1 + extreme_scaling)
     k_w_theory = k_c*(1 + mean_scaling - extreme_scaling)
 
     ΔT_global = 1 # no dT for now
     axtwin = cmip6_lib.pctl_plot(ax, ΔT_global, pctls, precip_pctls_c, precip_pctls_boot_c, k_c, θ_c, precip_pctls_w, precip_pctls_boot_w, k_w_theory, θ_w_theory)
-    # axtwin = cmip6_lib.pctl_plot(ax, ΔT_global, pctls, precip_pctls_c, precip_pctls_boot_c, k_c, θ_c, precip_pctls_w, precip_pctls_boot_w, k_w, θ_w)
 
     # label plot
-    ax.annotate("GPCP Monthly", (0.08, 0.8), xycoords="axes fraction")
+    ax.annotate(f"GPCP {freq.capitalize()}", (0.08, 0.8), xycoords="axes fraction")
     
     # temp change 
     # ax.annotate("$\Delta T = {:1.2f}$ K".format(ΔT), (0.08, 0.8), xycoords="axes fraction")
@@ -143,9 +190,34 @@ def plot_pctls():
     custom_handles = ["1979--2000", "2000--2020", "ratio", "gamma fit"]
     ax.legend(custom_lines, custom_handles)
 
-    plt.savefig(f"gpcp_{domain}_pctl.png")
-    # plt.savefig(f"gpcp_{domain}_pctl.pdf")
+    plt.savefig(f"gpcp_{domain}_{freq}_pctl.png")
+    print(f"gpcp_{domain}_{freq}_pctl.png")
+    plt.savefig(f"gpcp_{domain}_{freq}_pctl.pdf")
+    print(f"gpcp_{domain}_{freq}_pctl.pdf")
 
+def plot_hists():
+    """
+        plot_hists()
+        
+    Plot precip histograms for past/present. 
+    """
+    pctls, precip_c, precip_pctls_c, precip_pctls_boot_c, bins_c, hist_c, μ_c, σ_c, k_c, θ_c = load_data(f"data/gpcp_{domain}_{freq}_1979-2000.npz")
+    pctls, precip_w, precip_pctls_w, precip_pctls_boot_w, bins_w, hist_w, μ_w, σ_w, k_w, θ_w = load_data(f"data/gpcp_{domain}_{freq}_2000-2020.npz")
+
+    fig, ax = plt.subplots()
+    ax.loglog(bins_c[:-1], hist_c, c="tab:blue", label="1979--2000")
+    ax.loglog(bins_w[:-1], hist_w, c="tab:orange", label="2000--2020")
+
+    ax.legend()
+    ax.set_xlabel("Precipitation (mm day$^{-1}$)")
+    ax.set_ylabel("Probability")
+    ax.set_xlim([1e-5, 1e3])
+    ax.set_ylim([1e-13, 1e15])
+    plt.savefig(f"gpcp_{domain}_{freq}_hist.png")
+    print(f"gpcp_{domain}_{freq}_hist.png")
+    plt.savefig(f"gpcp_{domain}_{freq}_hist.pdf")
+    print(f"gpcp_{domain}_{freq}_hist.pdf")
+    plt.close()
 
 ################################################################################
 # run
@@ -155,6 +227,9 @@ def plot_pctls():
 pctl_min = 0.09
 pctl_max = 0.99999
 pctls = cmip6_lib.inv_x_transform(np.linspace(cmip6_lib.x_transform(pctl_min), cmip6_lib.x_transform(pctl_max), 100))
+
+# histogram bins
+nbins = 1000
 
 # lat slice
 latslice = slice(-90, 90)
@@ -175,11 +250,18 @@ elif latslice == "extrop":
 # path
 path = "/export/data1/hgpeterson/gpcp/"
 
+# monthly or daily
+# freq = "monthly"
+freq = "daily"
+
 # data files
-dfiles = path + "gpcp*.nc"
+dfiles = path + freq + "/gpcp*.nc"
 
 # get pctls
-# get_data_gpcp(dfiles, pctls, latslice, lonslice)
+# get_data(dfiles, pctls, nbins, latslice, lonslice)
 
 # plot pctls
-plot_pctls()
+# plot_pctls()
+
+# plot hists
+plot_hists()
